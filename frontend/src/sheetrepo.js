f3faf6dfbb8b45437bf86c9ccb1fd09a7251beaf
Alexandre MEYER <alexandre.meyer@iconsultants.fr>
2021-10-16 23:05:40 +0200

    still looks good

--- frontend/src/sheetrepo.js
+++ frontend/src/sheetrepo.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 export async function increment (row, col) {
-  console.log(`[SheetRepo] incrementing row ${row} col ${col}`)
+  console.log(`[SheetRepo] Incrementing row ${row} col ${col}`)
   const response = await axios.get(
     `http://localhost:5000/increment/${row}/${col}`
   )
-  console.log(response.data)
+  console.log(`[SheetRepo] ${response.data}`)
   return response.data
 }
