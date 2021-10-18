776e1ba719f9ac68999f492eea62488d10bf2874
Alexandre MEYER <alexandre.meyer@iconsultants.fr>
2021-10-18 23:27:35 +0200

    heroku app url

--- frontend/src/common/sheetrepo.js
+++ frontend/src/common/sheetrepo.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export async function increment (row, col) {
   console.log(`[SheetRepo] Incrementing row ${row} col ${col}`)
   const response = await axios.get(
-    `http://localhost:5000/increment/${row}/${col}`
+    `https://bourlyleague.herokuapp.com/increment/${row}/${col}`
   )
   console.log(`[SheetRepo] ${response.data}`)
   return response
@@ -12,7 +12,7 @@ export async function increment (row, col) {
 export async function kill (row, col, killed) {
   console.log(`[SheetRepo] Killing ${killed}`)
   const response = await axios.get(
-    `http://localhost:5000/kill/${row}/${col}/${killed}`
+    `https://bourlyleague.herokuapp.com/kill/${row}/${col}/${killed}`
   )
   console.log(`[SheetRepo] ${response.data}`)
   return response
@@ -21,7 +21,7 @@ export async function kill (row, col, killed) {
 export async function update (row, col, type, count) {
   console.log(`[SheetRepo] Updating ${type}`)
   const response = await axios.get(
-    `http://localhost:5000/update/${row}/${col}/${count}`
+    `https://bourlyleague.herokuapp.com/update/${row}/${col}/${count}`
   )
   console.log(`[SheetRepo] ${response.data}`)
   return response
